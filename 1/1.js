d4600279f046f2f9f281505ca1cb97f1cb734f9d
Reid Covington <reid@shopcube.com>
2014-07-15 12:42:53 -0400

    js v of prob 1 correct 1st crack

--- 1/1.js
+++ 1/1.js
@@ -2,3 +2,16 @@
 
 // # Find the sum of all the multiples of 3 or 5 below 1000.
 
+array = [];
+arrayLength = 1000;
+for ( var i = 0; i < arrayLength; i++ ){
+  if ( i % 3 === 0) {
+    array.push(i);
+  } else if ( i % 5 === 0){
+    array.push(i);
+  }
+}
+sum = 0;
+for ( var i = 0; i < array.length; i++ ){
+  sum += array[i];
+}
