d560e0bd2aa7864fca82adcb457cc3e281d0ff33
Reid Covington <reid@shopcube.com>
2014-07-15 12:49:27 -0400

    refactored optimized js prob 1

--- 1/1.js
+++ 1/1.js
@@ -2,16 +2,25 @@
 
 // # Find the sum of all the multiples of 3 or 5 below 1000.
 
-array = [];
-arrayLength = 1000;
-for ( var i = 0; i < arrayLength; i++ ){
-  if ( i % 3 === 0) {
-    array.push(i);
-  } else if ( i % 5 === 0){
-    array.push(i);
-  }
-}
-sum = 0;
-for ( var i = 0; i < array.length; i++ ){
-  sum += array[i];
+// first crack:
+// array = [];
+// arrayLength = 1000;
+// for ( var i = 0; i < arrayLength; i++ ){
+//   if ( i % 3 === 0) {
+//     array.push(i);
+//   } else if ( i % 5 === 0){
+//     array.push(i);
+//   }
+// }
+// sum = 0;
+// for ( var i = 0; i < array.length; i++ ){
+//   sum += array[i];
+// }
+
+// refactored:
+var sum = 0;
+for (var i = 1; i < 1000; i++) {
+    if (((i % 3) == 0) || ((i % 5) == 0)) {
+        sum += i;
+    }
 }
